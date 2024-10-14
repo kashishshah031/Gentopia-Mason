2628afe5ddd2d27496f0a855d89b9a201cb294d6
kashishshah031 <kashishshah031@gmail.com>
2024-10-13 22:30:22 -0400

    First Commit - Assignment 2

--- GentPool/gentpool/pool/__init__.py
+++ GentPool/gentpool/pool/__init__.py
@@ -9,3 +9,6 @@ from .vanilla_template import *
 from .openai_memory_template import *
 from .vanilla-ziyu import *
 from .scholar-ziyu import *
+from .assign2_part1 import *
+from .assign2_part2 import *
+from .assign2_part2 import *
